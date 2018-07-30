54565f181fd9a944e053336e863fa5456890a497
nanhu <nan.hu@tieto.com>
2018-07-30 09:10:47 +0200

    update mian_sim for SCL L

--- main_sim.m
+++ main_sim.m
@@ -1,127 +1,48 @@
-
-
 clear; clc; close all;
 
- 
+N = 16;
+K = 8;
 
-N = 16 ;
-
-K = 8 ;
-
- 
-
-%global EbN0
+%N = 1024 ;
+%K = 1000 ;
 
 EbN0 = 1:0.5:4;
+%EbN0 = 0.5:0.5:2.5;
 
 nMaxBlks = 1e4;
-
 tarErrs = 100;
+nErr  = zeros(3, length(EbN0));
+nBlks = nMaxBlks*ones(3, length(EbN0));
 
-nErr  = double(zeros(1, length(EbN0)));
-
-nBlks = nMaxBlks*ones(1, length(EbN0));
-
- 
-
-for ebn0 = 1:length(EbN0)  %SNR
-
-    for k = 1:nMaxBlks     %number of Blocks
-
+for ebn0 = 1:length(EbN0)
+    for k = 1:nMaxBlks
         info_bit = randi([0,1],1,K);
-
         code_bit = polar_encoder(info_bit, N);
-
         unit_noise = randn(1,N);
-
         en = 10^(EbN0(ebn0)/10);
-
-        sigma = 1/sqrt(2*en*K/N); % sigma=E/sqrt(2*SNR)
-
+        sigma = 1/sqrt(2*en*K/N);
         x = (1-2*code_bit) + sigma*unit_noise;
-       %dec = polar_decoder(x, K, 1);
-
-        %nErr(1,ebn0) = nErr(1,ebn0) + ((dec-info_bit)*(dec-info_bit)'>0);
-
-        %dec = polar_decoder(x, K, 2);
-
-        %nErr(2,ebn0) = nErr(2,ebn0) + ((dec-info_bit)*(dec-info_bit)'>0);
-
-        %dec = polar_decoder(x, K, 8);
-
-        %nErr(3,ebn0) = nErr(3,ebn0) + ((dec-info_bit)*(dec-info_bit)'>0);
-
-       dec = polar_decoder_sc(x, K);
-
-       nErr(ebn0) = nErr(ebn0) + ((dec-info_bit)*(dec-info_bit)'>0);
-%   fprintf('nErr(ebn0) =%d, (dec-info_bit)*(dec-info_bit)=%d ',nErr(ebn0) ,(dec-info_bit)*(dec-info_bit)');
-       if((dec-info_bit)*(dec-info_bit)'==0)
-
-          
-          
-          fprintf('\n\n ==================this block BLER is 0 EbN0 = %0.1fdB  ==================\n',EbN0(ebn0));
-          
-          fprintf('k=%d    ',k);
-
-          fprintf('BLER     :  ');
-
-          fprintf('%0.2e    ',nErr(ebn0)/k);
-
-       end
-
-       % if mod(k,10) == 0
-
-        %    fprintf('\n\n ================== EbN0 = %0.1fdB  ==================\n',EbN0(ebn0));
-
-        %    fprintf('List Size:     1            2           8\n');
-
-        %    fprintf('BLER     :  ');
-
-        %    for n = 1:3
-
-        %        fprintf('%0.2e    ',nErr(n, ebn0)/k);
-
-         %   end
-
-        %end
+        dec = polar_decoder_scl(x, K, 1);
+        nErr(1,ebn0) = nErr(1,ebn0) + ((dec-info_bit)*(dec-info_bit)'>0);
+        dec = polar_decoder_scl(x, K, 2);
+        nErr(2,ebn0) = nErr(2,ebn0) + ((dec-info_bit)*(dec-info_bit)'>0);
+        dec = polar_decoder_scl(x, K, 16);
+        nErr(3,ebn0) = nErr(3,ebn0) + ((dec-info_bit)*(dec-info_bit)'>0);
 
         if mod(k,10) == 0
-
-          fprintf('\n\n ================== EbN0 = %0.1fdB  ==================\n',EbN0(ebn0));
-
-           %fprintf('List Size:     1            2           8\n');
-
+            fprintf('\n\n ================== EbN0 = %0.1fdB  ==================\n',EbN0(ebn0));
+            fprintf('List Size:     1            2           8\n');
             fprintf('BLER     :  ');
-
- 
-
-           fprintf('%0.2e    ',nErr(ebn0)/k);
-
+            for n = 1:3
+                fprintf('%0.2e    ',nErr(n, ebn0)/k);
+            end
         end
-
-        %end
-
-       % if nErr(1,ebn0) > tarErrs && nErr(2,ebn0) > tarErrs && nErr(3,ebn0) > tarErrs
-
-       %     nBlks(:,ebn0) = k;
-
-        %    break;
-
-        if nErr(ebn0) > tarErrs
-
-              nBlks(:,ebn0) = k;
-
-              break;
-
+        if nErr(1,ebn0) > tarErrs && nErr(2,ebn0) > tarErrs && nErr(3,ebn0) > tarErrs
+            nBlks(:,ebn0) = k;
+            break;
         end
-
     end
-
 end
 
- 
-%BLER = zeros(1,length(nBlks));
-
 BLER = nErr./nBlks;
-
 semilogy(EbN0, BLER, 'o-'); grid on;
